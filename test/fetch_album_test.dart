b66958ec4bdacdd605310f0be2486d4a0ee43cf8
IbrokhimMirzaev <mirzaevibrokhim@gmail.com>
2022-12-04 13:30:12 +0500

    some changes

--- test/fetch_album_test.dart
+++ test/fetch_album_test.dart
@@ -15,6 +15,8 @@ void main() {
     test('returns an Album if the http call completes successfully', () async {
       final client = MockClient();
 
+      // Use Mockito to return a successful response when it calls the
+      // provided http.Client.
       when(client
           .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1')))
           .thenAnswer((_) async =>
@@ -26,6 +28,8 @@ void main() {
     test('throws an exception if the http call completes with an error', () {
       final client = MockClient();
 
+      // Use Mockito to return an unsuccessful response when it calls the
+      // provided http.Client.
       when(client
           .get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1')))
           .thenAnswer((_) async => http.Response('Not Found', 404));
